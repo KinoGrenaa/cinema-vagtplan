7473acefc961d1a4f6b15423763b083b00c0f77d
KinoGrenaa <post@kinogrenaa.dk>
2026-07-11 22:38:52 +0200

    Organize payroll master cinema layout

--- frontend/app/(app)/payroll/page.tsx
+++ frontend/app/(app)/payroll/page.tsx
@@ -2,7 +2,7 @@
 
 import PermissionGuard from "@/app/components/PermissionGuard";
 
-import PayrollMasterCinemaRequired from "./components/PayrollMasterCinemaRequired";
+import PayrollMasterCinemaRequired from "./components/layout/PayrollMasterCinemaRequired";
 import PayrollPageContent from "./components/PayrollPageContent";
 import { usePayrollPage } from "./hooks/usePayrollPage";
 
